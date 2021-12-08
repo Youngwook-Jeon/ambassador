888ba79cac4b840394b04fc08b3fbadfe2a2c673
Young wook Jeon <ela87@naver.com>
2021-12-08 23:02:22 +0900

    Create links

--- nest-ambassador/src/link/link.controller.ts
+++ nest-ambassador/src/link/link.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { LinkService } from './link.service';
+import { Request } from 'express';
 
 @Controller()
 export class LinkController {
-  constructor(private linkService: LinkService) {}
+  constructor(
+    private linkService: LinkService,
+    private authService: AuthService,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Get('admin/users/:id/links')
@@ -12,6 +25,17 @@ export class LinkController {
     return this.linkService.find({
       user: id,
       relations: ['orders'],
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('ambassador/links')
+  async create(@Body('products') products: number[], @Req() request: Request) {
+    const user = await this.authService.user(request);
+    return this.linkService.save({
+      code: Math.random().toString(36).substr(6),
+      user,
+      products: products.map((id) => ({ id })),
     });
   }
 }

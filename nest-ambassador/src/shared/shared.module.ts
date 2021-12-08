3fe4d1527fe3c99414ab98f755723cd8b2a9b255
Young wook Jeon <ela87@naver.com>
2021-12-08 20:38:45 +0900

    Add dependencies of redis

--- nest-ambassador/src/shared/shared.module.ts
+++ nest-ambassador/src/shared/shared.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'secret',
       signOptions: { expiresIn: '1d' },
+    }),
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
     }),
   ],
   exports: [JwtModule],
